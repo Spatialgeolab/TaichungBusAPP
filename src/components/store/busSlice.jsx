6e69027a89914ac4bf230bcc1b7b17b5a48da217
SHIHTENG <alt41450@gmail.com>
2023-12-02 04:51:56 +0800

    Redux管理公車所有狀態

--- src/components/store/busSlice.jsx
+++ src/components/store/busSlice.jsx
@@ -1,32 +1,35 @@
-import { createSlice} from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 
 //使用rtk構建Slice_Store 傳入obj作為參數
 const busSlice = createSlice({
   name: "bus",
   initialState: {
-    busId:null,
+    busId: null,
     busPosition: [],
-    busDetail: [],
+    busDetail: [],          
     busRoute: [],
-    busRouteList:[],
+    busRouteList: [],
     busStops: [],
     busDirection: 0,
+    busListAll: [],
   },
   reducers: {
     setBusObj(state, action) {
-      console.log('action',action,state)
       // state是原本state的代理對象 => 不需要shallow copy
-      state.busId=action.payload.busId
-      state.busPosition=action.payload.busPosition
-      state.busDetail=action.payload.busDetail
-      state.busRoute=action.payload.busRoute
-      state.busRouteList=action.payload.busRouteList
-      state.busStops=action.payload.busStops
-      state.busDirection=action.payload.busDirection
+      state.busId = action.payload.busId;
+      state.busPosition = action.payload.busPosition;
+      state.busDetail = action.payload.busDetail;
+      state.busRoute = action.payload.busRoute;
+      state.busRouteList = action.payload.busRouteList;
+      state.busStops = action.payload.busStops;
+      state.busDirection = action.payload.busDirection;
+    },
+    setBusListAll(state, action) {
+      state.busListAll = action.payload.busListAll;
     },
   },
 });
 
 // creatSlice 會自動設置type
-export const { setBusObj } = busSlice.actions;
-export const {reducer:busReducer}=busSlice
+export const { setBusObj, setBusListAll } = busSlice.actions;
+export const { reducer: busReducer } = busSlice;
