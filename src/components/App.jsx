b53e316519272135a76b5a1e8427dfdca372d9bb
SHIHTENG <alt41450@gmail.com>
2023-12-02 04:48:32 +0800

    拆分部分組件

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,54 +1,209 @@
-import React from "react";
+// @ts-nocheck
 import axios from "axios";
 import "leaflet/dist/leaflet.css";
-import { useEffect, useState,useRef,useCallback} from 'react';
-import {Route,Routes} from "react-router-dom";
-import {MapContainer,TileLayer, Marker, Popup, Polyline } from "react-leaflet" 
-import { getApiUrl,busMapApi } from "./api/apiConfig";
+import { useEffect, useState, useRef, useCallback } from "react";
+import { Route, Routes } from "react-router-dom";
+import { MapContainer, TileLayer, Marker, Popup, Polyline } from "react-leaflet";
 import SearchBox from "./SearchBox";
-import RouteDetail  from "./RouteDetail";
+import RouteDetail from "./RouteDetail";
 import RouteNav from "./RouteNav";
-import Button from 'react-bootstrap/Button';
-import {customIcon} from "./api/customIcon";
+import OtherRoute from "./OtherRoute";
+import Lmap from "./Lmap";
+import { customIcon } from "./api/customIcon";
 import { setBusObj } from "./store/busSlice";
-import { useSelector,useDispatch } from "react-redux";
-import { useGetBusPositionQuery,useGetBusRouteQuery,useGetBusStopQuery,useGetBusRouteListQuery,useGetBusRouteDetailQuery} from "./store/busApi";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  useGetBusPositionQuery,
+  useGetBusRouteQuery,
+  useGetBusStopQuery,
+  useGetBusRouteListQuery,
+  useGetBusRouteDetailQuery,
+} from "./store/busApi";
 import { useGetTokenQuery } from "./api/authApi";
 import { authSetToken } from "./store/authSlice";
-const App = () => {
-    // useSelector() 載入state數據
-    const {token} = useSelector(state=>state.token)
-    const dispatch = useDispatch()
-    // 以上練習redux
-    const routeUrl = getApiUrl(busMapApi.route);
-    const stopUrl = getApiUrl(busMapApi.stop);
-    const busUrl = getApiUrl(busMapApi.realTime);
-    const routeListUrl = getApiUrl(busMapApi.routeList);
-    const routeDetailUrl = getApiUrl(busMapApi.routeDetail);
-    const mapRef = useRef(null); // 地圖元素引用
-    const [routeDetail,setRouteDetail]=useState([]) //使用useState來維護路線資訊的狀態
-    const [routes, setRoutes] = useState([]); // 使用useState來維護路線的狀態
-    const [stops, setStops] = useState([]); // 使用useState來維護站牌的狀態
-    const [bus, setBus] = useState([]); // 使用useState來維護公車定位的狀態
-    const [stopName,setStopName]=useState('')// 使用useState來維護站位的狀態
-    const [inputBus,setInputBus] =useState('1')//使用useState來當前查詢路線的狀態
-    const [direction,setDirection]=useState(0)
-    const [otherRouteDeatil,setOtherRouteDeatil]=useState([{}])
-    const lineOptions = { color: 'blue',dashArray: '10, 10'}
-    const [isGetToken,setisGetToken] =useState(Boolean(token))
-    if (!isGetToken) {
-        console.log('沒有token進行獲取')
-        const {data:tokenData, isLoading, isFetching } = useGetTokenQuery()
-        // 將數據派發到 Redux store
-        if(tokenData){
-            dispatch(authSetToken({
-                token:tokenData.access_token}));
-            setisGetToken(true)
-        }
+export default function App() {
+  // useSelector() 載入state數據
+  const { token } = useSelector((state) => state.token);
+  const { busPosition, busDetail, busRoute, busRouteList, busStops } = useSelector(
+    (state) => state.bus
+  );
+  const dispatch = useDispatch();
+  // 以上練習redux
+  const mapRef = useRef(null); // 地圖元素引用
+  const [routeDetail, setRouteDetail] = useState([]); //使用useState來維護路線資訊的狀態
+  const [stopName, setStopName] = useState(""); // 使用useState來維護站位的狀態
+  const [inputBus, setInputBus] = useState("1"); //使用useState來當前查詢路線的狀態
+  const [direction, setDirection] = useState(0);
+  const lineOptions = { color: "blue", dashArray: "10, 10" };
+  const [isGetToken, setisGetToken] = useState(Boolean(token));
+  const [isUpdateData, setIsUpdateData] = useState(true);
+  if (!isGetToken) {
+    console.log("沒有token，進行獲取");
+    const { data: tokenData, isLoading, isFetching } = useGetTokenQuery();
+    // 將數據派發到 Redux store
+    if (tokenData) {
+      dispatch(
+        authSetToken({
+          token: tokenData.access_token,
+        })
+      );
+      setisGetToken(true);
     }
-    return (
-      <></>
-  )
-};
+  }
+  const formatRouteLine = (res, direction) => {
+    let routeNodes = res;
+    console.log(direction);
+    routeNodes = routeNodes[direction].Geometry.replace("LINESTRING(", "")
+      .replace(")", "")
+      .split(",");
+    let routeLine = routeNodes.map((node) => [node.split(" ")[1], node.split(" ")[0]]);
+    return [routeLine];
+  };
+  const formatRoutestops = (res, direction) => {
+    let routeStops = res;
+    // console.log('查詢路線:',routeName,'| 回傳站點資料:',routeStops)
+    routeStops = routeStops[direction].Stops.map((stops) => {
+      return {
+        StopName: stops.StopName.Zh_tw,
+        PositionLat: stops.StopPosition.PositionLat,
+        PositionLon: stops.StopPosition.PositionLon,
+      };
+    });
+    // 計算公車路線中心
+    let startStop = routeStops[0];
+    let endStop = routeStops[routeStops.length - 1];
+    let map = mapRef.current;
+    if (map) {
+      map.setView(
+        [
+          (startStop.PositionLat + endStop.PositionLat) / 2,
+          (startStop.PositionLon + endStop.PositionLon) / 2,
+        ],
+        12.5
+      );
+    }
+    return routeStops;
+  };
+  const formatBusLocation = (res, direction) => {
+    let busLocations = res;
+    busLocations = busLocations.filter((item) => item.Direction === direction);
+    return [...busLocations];
+  };
+  const formatRouteDetail = (res, direction) => {
+    let routeDeatil = res.filter((route) => route.Direction === parseInt(direction));
+    // 進行站點排序
+    routeDeatil.sort((a, b) => a.StopSequence - b.StopSequence);
+    // console.log('查詢路線:',routeName,"回傳路線資料:",routeDeatil)
+    return routeDeatil.map((item) => {
+      return {
+        StopName: item.StopName.Zh_tw,
+        StopSequence: item.StopSequence,
+        StopStatus: item.StopStatus,
+        NextBusTime: item.NextBusTime,
+        StopID: item.StopID,
+        EstimateTime: item.EstimateTime,
+        PlateNumb: item.PlateNumb,
+      };
+    });
+  };
+  const busData = [
+    useGetBusPositionQuery(inputBus, direction),
+    useGetBusRouteDetailQuery(inputBus, direction),
+    useGetBusRouteQuery(inputBus, direction),
+    useGetBusRouteListQuery(inputBus, direction),
+    useGetBusStopQuery(inputBus, direction),
+  ];
+  const updateData = () => {
+    for (let i of busData) {
+      i.refetch();
+    }
+  };
+  if (busData.filter((item) => item.status == "fulfilled").length == 5 && isUpdateData) {
+    console.log("開始派發數據");
+    dispatch(
+      setBusObj({
+        busId: inputBus,
+        busPosition: formatBusLocation(busData[0].data, direction),
+        busDetail: formatRouteDetail(busData[1].data, direction),
+        busRoute: formatRouteLine(busData[2].data, direction),
+        busRouteList: busData[3].data,
+        busStops: formatRoutestops(busData[4].data, direction),
+        busDirection: direction,
+      })
+    );
+    setIsUpdateData(false);
+  }
+  useEffect(() => setIsUpdateData(true), [inputBus]);
 
-export default App;
+  const formattedTime = useCallback((time) => {
+    const datetimeString = time;
+    const dateObj = new Date(datetimeString);
+    // 提取小時和分鐘部分
+    const hours = dateObj.getHours();
+    const minutes = dateObj.getMinutes();
+    // 格式化時間（加0是為了確保時間以兩位數表示）
+    const formattedTime = `${String(hours).padStart(2, "0")}:${String(minutes).padStart(2, "0")}`;
+    return formattedTime;
+  });
+
+  return (
+    <>
+      {/* 獲取到token後再進行mount */}
+      {isGetToken && (
+        <>
+          <nav class='navbar navbar-light bg-light'>
+            <h1 className='text-dark d-block w-100 text-center'>台中市即時公車地圖</h1>
+          </nav>
+          <div className='container-fluid'>
+            {/* 響應式設計 > */}
+            <div class='row flex-column-reverse flex-sm-row'>
+              <div class='col-12 col-sm-4'>
+                <div className='container'>
+                  <SearchBox inputBus={inputBus} setInputBus={setInputBus} />
+                  <RouteNav setDirection={setDirection} direction={direction} />
+                  <Routes>
+                    <Route
+                      path='/'
+                      element={
+                        <RouteDetail
+                          routeDetail={busDetail}
+                          direction={direction}
+                          mapRef={mapRef}
+                          inputBus={inputBus}
+                          updateData={updateData}
+                          stops={busStops}
+                          formattedTime={formattedTime}
+                          setIsUpdateData={setIsUpdateData}
+                        />
+                      }
+                    />
+                    <Route
+                      path='/inbound'
+                      element={
+                        <RouteDetail
+                          routeDetail={busDetail}
+                          direction={direction}
+                          mapRef={mapRef}
+                          inputBus={inputBus}
+                          updateData={updateData}
+                          stops={busStops}
+                          formattedTime={formattedTime}
+                          setIsUpdateData={setIsUpdateData}
+                        />
+                      }
+                    />
+                  </Routes>
+                </div>
+              </div>
+              <div class='col-12 col-sm-8'>
+                <div className='main-content'>
+                  <Lmap propsObj={{ inputBus, direction, setInputBus, formattedTime, mapRef }} />
+                </div>
+              </div>
+            </div>
+          </div>
+        </>
+      )}
+    </>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Busmap from './components/map.jsx'
-import './index.css'
-import { BrowserRouter} from "react-router-dom";
-import store from './components/store/index.js';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Busmap from "./components/map.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import store from "./components/store/index.js";
 import { Provider } from "react-redux";
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <Provider store={store} >
-        <BrowserRouter>
-            <Busmap />  
-        </BrowserRouter>
-    </Provider>
-)
+import App from "./components/App.jsx";
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+);
