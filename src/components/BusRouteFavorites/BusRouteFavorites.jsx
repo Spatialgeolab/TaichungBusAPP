f1f9a92b39cdcb565254b397637de94f37e74207
SHIHTENG <alt41450@gmail.com>
2023-12-07 02:13:50 +0800

    change style

--- src/components/BusRouteFavorites/BusRouteFavorites.jsx
+++ src/components/BusRouteFavorites/BusRouteFavorites.jsx
@@ -2,21 +2,38 @@ import "./BusRouteFavorites.css";
 import BackDrop from "../BackDrop/BackDrop";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar } from "@fortawesome/free-regular-svg-icons";
-import { faStar as faStarSolid } from "@fortawesome/free-solid-svg-icons";
-const BusRouteFavorites = ({ setShowFavorite, busFavoriteItem, addFavoriteRoute, setInputBus }) => {
+import { faStar as faStarSolid, faChevronCircleLeft } from "@fortawesome/free-solid-svg-icons";
+const BusRouteFavorites = ({
+  setShowFavorite,
+  busFavoriteItem,
+  addFavoriteRoute,
+  setInputBus,
+  clearFavoriteRoute,
+}) => {
   return (
     // BD透過react portal渲染其InnerText
     <BackDrop setShowFavorite={setShowFavorite}>
       <div className='favorite-container'>
-        <span
-          className='close-btn'
-          onClick={() => {
-            setShowFavorite(false);
-          }}>
-          X
-        </span>
-        <h2>我的收藏路線</h2>
-        <br />
+        <div className='fav-header'>
+          <FontAwesomeIcon
+            className='close-btn'
+            size='xl'
+            icon={faChevronCircleLeft}
+            onClick={() => {
+              setShowFavorite(false);
+            }}
+          />
+          <div>
+            <span>我的收藏路線</span>
+            <span
+              onClick={() => {
+                clearFavoriteRoute();
+              }}>
+              清空路線
+            </span>
+          </div>
+          <hr />
+        </div>
         <ul className='favorite-ul'>
           {busFavoriteItem.map((item) => {
             return (
