ff3e1e4b33b14b3c6070d6e3e5ba3665091462db
SHIHTENG <alt41450@gmail.com>
2023-12-08 02:37:14 +0800

    add commits

--- src/components/store/busApi.js
+++ src/components/store/busApi.js
@@ -18,6 +18,7 @@ const busApi = createApi({
   //endpoints指定API中的各種方法 build構造器
   endpoints(build) {
     return {
+      // 公車目前位置
       getBusPosition: build.query({
         query(busId) {
           // 請求的子路徑
@@ -25,30 +26,35 @@ const busApi = createApi({
           return busMapApi.realTime + queryString;
         },
       }),
+      //公車路線線型
       getBusRoute: build.query({
         query(busId) {
           const queryString = `?$format=JSON&$filter=RouteName/Zh_tw eq '${busId}'`;
           return busMapApi.route + queryString;
         },
       }),
+      //公車站牌位置
       getBusStop: build.query({
         query(busId) {
           const queryString = `?$format=JSON&$filter=RouteName/Zh_tw eq '${busId}'`;
           return busMapApi.stop + queryString;
         },
       }),
+      //目前路線的站點資訊
       getBusRouteList: build.query({
         query(busId) {
           const queryString = `?$format=JSON&$filter=RouteName/Zh_tw eq '${busId}'`;
           return busMapApi.routeList + queryString;
         },
       }),
+      // 目前路線的進站狀態
       getBusRouteDetail: build.query({
         query(busId) {
           const queryString = `?$format=JSON&$filter=RouteName/Zh_tw eq '${busId}'`;
           return busMapApi.routeDetail + queryString;
         },
       }),
+      //目前路線的進站狀態
       getBusRouteDetailByStop: build.query({
         query(Stop) {
           const queryString = `?$format=JSON&$filter=StopName/Zh_tw eq '${Stop}'`;
